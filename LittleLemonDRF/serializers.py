18b9d1ebbc335a4c563fce7b2d8a40118aa79ad5
OyedunOye <oyesinaoyedun@yahoo.com>
2024-01-23 22:15:23 +0100

    serializers for models started

--- LittleLemonDRF/serializers.py
+++ LittleLemonDRF/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import MenuItem, Category, Rating
+from .models import MenuItem, Category, Cart, Order, OrderItem
 from rest_framework.validators import UniqueTogetherValidator
 from django.contrib.auth.models import User
 
@@ -11,27 +11,52 @@ class CategorySerializer (serializers.ModelSerializer):
 class MenuItemSerializer(serializers.ModelSerializer):
     category_id = serializers.IntegerField(write_only=True)
     category = CategorySerializer(read_only=True)
+    
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'price', 'inventory', 'category', 'category_id']
+        fields = ['id', 'title', 'price', 'featured', 'category', 'category_id']
         
-class RatingSerializer (serializers.ModelSerializer):
+class CartSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(
             queryset=User.objects.all(),
             default=serializers.CurrentUserDefault()
     )
-
     class Meta:
-        model = Rating
-        fields = ['user', 'menuitem_id', 'rating']
+        model = Cart
+        fields = ['user', 'menuitem', 'quantity', 'unit_price', 'price']
+        
+        
+class OrderSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+            queryset=User.objects.all(),
+            default=serializers.CurrentUserDefault()
+    )
+    class Meta:
+        model = Order
+        fields = ['user', 'delivery_crew', 'status', 'total', 'date']
+    
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['order', 'menuitem', 'quantity', 'unit_price', 'price']
+        
+# class RatingSerializer (serializers.ModelSerializer):
+#     user = serializers.PrimaryKeyRelatedField(
+#             queryset=User.objects.all(),
+#             default=serializers.CurrentUserDefault()
+#     )
 
-        validators = [
-            UniqueTogetherValidator(
-                queryset=Rating.objects.all(),
-                fields=['user', 'menuitem_id']
-            )
-        ]
+#     class Meta:
+#         model = Rating
+#         fields = ['user', 'menuitem_id', 'rating']
 
-        extra_kwargs = {
-            'rating': {'min_value': 0, 'max_value':5},
-        }
+#         validators = [
+#             UniqueTogetherValidator(
+#                 queryset=Rating.objects.all(),
+#                 fields=['user', 'menuitem_id']
+#             )
+#         ]
+
+#         extra_kwargs = {
+#             'rating': {'min_value': 0, 'max_value':5},
+#         }
