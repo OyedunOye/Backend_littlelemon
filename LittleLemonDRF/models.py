99c6bdedc35503d87fe358dd201b16a6cfef0928
OyedunOye <oyesinaoyedun@yahoo.com>
2024-01-25 23:50:29 +0100

    modifications to cart model

--- LittleLemonDRF/models.py
+++ LittleLemonDRF/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
 class MenuItem(models.Model):
     title=models.CharField(max_length=255, db_index=True)
     price=models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
-    featured=models.BooleanField(db_index=True)
+    featured=models.BooleanField(db_index=True, default=False)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     
 class Cart(models.Model):
@@ -17,7 +17,7 @@ class Cart(models.Model):
     menuitem= models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.SmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    price = models.DecimalField(max_digits=2, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     
     class Meta:
         unique_together = ('menuitem', 'user')
@@ -30,7 +30,7 @@ class Order(models.Model):
     date = models.DateField(db_index=True)
     
 class OrderItem(models.Model):
-    order = models.ForeignKey(User, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.SmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
