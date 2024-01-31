a60faad31ad3bd22b3e95c135db18e7dd243a3db
OyedunOye <oyesinaoyedun@yahoo.com>
2024-01-31 23:44:42 +0100

    update to views

--- LittleLemonDRF/views.py
+++ LittleLemonDRF/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from .models import MenuItem, Category, Rating
-from .serializers import MenuItemSerializer, CategorySerializer, RatingSerializer
+from .models import MenuItem, Category, Cart, Order, OrderItem
+from .serializers import MenuItemSerializer, CategorySerializer, CartSerializer, OrderSerializer, OrderItemSerializer
 
 class CategoriesView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
@@ -10,14 +10,29 @@ class CategoriesView(generics.ListCreateAPIView):
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    ordering_fields = ['price', 'inventory']
-    filterset_fields = ['price', 'inventory']
+    ordering_fields = ['price']
+    filterset_fields = ['price']
     search_fields = ['category']
+     
+class CartView(generics.ListCreateAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
     
-class RatingsView(generics.ListCreateAPIView):
-    queryset = Rating.objects.all()
-    serializer_class = RatingSerializer
+   
+class OrderView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    
+    def get_permissions(self):
+        if(self.request.method=='GET'):
+            return []
 
+        return [IsAuthenticated()]
+    
+class OrderItemView(generics.ListCreateAPIView):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+    
     def get_permissions(self):
         if(self.request.method=='GET'):
             return []
