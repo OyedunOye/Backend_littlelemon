9b163f1fd297c2905daee87b61742d0f609ff77d
OyedunOye <oyesinaoyedun@yahoo.com>
2024-02-02 23:49:54 +0100

    chahnges to urls file in app

--- LittleLemonDRF/urls.py
+++ LittleLemonDRF/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('category', views.CategoriesView.as_view()),
     path('menu-items', views.MenuItemsView.as_view()),
-    path('ratings', views.RatingsView.as_view()),
+    path('cart', views.CartView.as_view()),
+    path('order', views.OrderView.as_view()),
+    path('order-item', views.OrderItemView.as_view()),
+    # path('users', views.User.),
+    path('api-token-auth', obtain_auth_token),
 ]
