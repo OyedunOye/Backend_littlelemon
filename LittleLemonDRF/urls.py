58eaa61d06734cd2cbc9e567b1a225518f4d7d80
OyedunOye <oyesinaoyedun@yahoo.com>
2024-02-18 14:02:28 +0100

    update to app urls

--- LittleLemonDRF/urls.py
+++ LittleLemonDRF/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 from . import views
-from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('category', views.CategoriesView.as_view()),
     path('menu-items', views.MenuItemsView.as_view()),
-    path('cart', views.CartView.as_view()),
-    path('order', views.OrderView.as_view()),
-    path('order-item', views.OrderItemView.as_view()),
-    # path('users', views.User.),
-    path('api-token-auth', obtain_auth_token),
+    path('menu-items/<int:pk>', views.SingleMenuView.as_view()),
+    path('groups/manager/users', views.ManagerUsersView.as_view()),
+    path('groups/manager/users/<int:pk>', views.SingleManagerUserView.as_view()),
+    path('groups/delivery-crew/users', views.DeliveryCrewUsersView.as_view()),
+    path('groups/delivery-crew/users/<int:pk>', views.SingleDeliveryCrewUserView.as_view()),
+    path('cart/menu-items', views.CustomerCartView.as_view()),
+    path('orders', views.OrdersView.as_view()),
+    path('cart/orders', views.OrdersView.as_view()),
+    path('orders/<int:pk>', views.SingleOrderView.as_view()),
+    path('cart/orders/<int:pk>', views.SingleOrderView.as_view()),
+    # path('api-token-auth', obtain_auth_token),
 ]
